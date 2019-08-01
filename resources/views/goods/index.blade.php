8fa81d51b4376983a8806563e125c644195160cd
root <root@ip-172-31-47-190.ap-northeast-1.compute.internal>
2019-08-01 13:31:11 +0000

    [update] add comma into goods price

--- resources/views/goods/index.blade.php
+++ resources/views/goods/index.blade.php
@@ -19,6 +19,8 @@
 		{{ csrf_field() }}
 		<tr> 
 			<td><img class="good-image" src="/storage/{{ $good->imagePath }}" ></td>
+			<!-- 商品タイトルは20文字，商品説明は50文字で折り返すように改行コードを挿入する -->
+			<!-- 商品価格は見やすさのために3桁ごとにカンマを付ける -->
 			@php
 				$title = $good->title;
 				$lineWidth = 20;
@@ -26,16 +28,24 @@
 				for($i = 1; $i <= $num; $i++) {
 					$title = substr_replace($title, "\n", ($i*$lineWidth + $i - 1), 0);
 				}
+
 				$desc = $good->desc;
 				$lineWidth = 50;
 				$num = strlen($desc)/$lineWidth;
 				for($i = 1; $i <= $num; $i++) {
 					$desc = substr_replace($desc, "\n", ($i*$lineWidth + $i - 1), 0);
 				}
+
+				$price = strval($good->price);
+				$commaWidth = 3;
+				$num = (strlen($price) - 1)/$commaWidth;
+				for($i = 1; $i <= $num; $i++) {
+					$price = substr_replace($price, ",", (strlen($price) - ($i*$commaWidth + $i - 1)), 0);
+				}
 			@endphp
 			<td> {!! nl2br(e($title)) !!} </td>
 			<td> {!! nl2br(e($desc))  !!} </td>
-			<td> {{ $good->price }}円 </td>
+			<td> {{ $price }}円 </td>
 			<td> 
 				<form action="/goods/{{ $good->id }}/edit" method="get">
 					<button> 商品編集 </button>
