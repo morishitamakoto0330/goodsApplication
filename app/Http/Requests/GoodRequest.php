e23caf47f3d9765761d0ce5c980820cef41b91cb
root <root@ip-172-31-47-190.ap-northeast-1.compute.internal>
2019-08-01 04:19:26 +0000

    [update] validation check

--- app/Http/Requests/GoodRequest.php
+++ app/Http/Requests/GoodRequest.php
@@ -43,13 +43,13 @@ class GoodRequest extends FormRequest
     public function messages()
     {
 	    return [
-		    'image.required' => '商品画像を選んで下さい',
-		    'image.image' => '画像ファイルを選んで下さい',
-		    'image.mimes' => '商品画像の拡張子はjpegもしくはpngにして下さい',
-		    'title.required' => '商品名を記入してください',
-		    'desc.required'  => '商品説明を記入して下さい',
-		    'price.required' => '商品価格を設定して下さい',
-		    'price.integer' => '商品価格は整数で設定して下さい',
+		    'image.required' => '※商品画像を選んで下さい',
+		    'image.image' => '※画像ファイルを選んで下さい',
+		    'image.mimes' => '※商品画像の拡張子はjpegもしくはpngにして下さい',
+		    'title.required' => '※商品タイトルを記入してください',
+		    'desc.required'  => '※商品説明を記入して下さい',
+		    'price.required' => '※商品価格を設定して下さい',
+		    'price.integer' => '※商品価格は整数で設定して下さい',
 	    ];
     }
 }
--- resources/views/goods/create.blade.php
+++ resources/views/goods/create.blade.php
@@ -12,23 +12,73 @@
 		{{ csrf_field() }}
 
 		@if ($errors->has('image'))
-			<tr> <th> </th> <td> <p class="error-message"> {{ $errors->first('image') }} </p> </td></tr>
+			@php
+				$errorsImage = $errors->get('image');
+			@endphp
+			<tr> 
+				<th>
+				</th>
+				<td>
+				@foreach ($errorsImage as $errorImage)
+					<p class="error-message"> {{ $errorImage }} </p>
+				@endforeach
+				</td>
+			</tr>
 		@endif
 		<tr> <th>　　商品画像：</th> <td><input type="file" name="image" value=""></td></tr>
+
+
 		@if ($errors->has('title'))
-			<tr> <th> </th> <td>{{ $errors->first('title') }}</td></tr>
+			@php
+				$errorsTitle = $errors->get('title');
+			@endphp
+			<tr>
+				<th>
+				</th>
+				<td>
+				@foreach ($errorsTitle as $errorTitle)
+					<p class="error-message"> {{ $errorTitle }} </p>
+				@endforeach
+				</td>
+			</tr>
 		@endif
 		<tr> <th>商品タイトル：</th> <td><input type="text" name="title" value="{{ old('title') }}"></td></tr>
+
+
 		@if ($errors->has('desc'))
-			<tr> <th> </th> <td>{{ $errors->first('desc') }}</td></tr>
+			@php
+				$errorsDesc = $errors->get('desc');
+			@endphp
+			<tr>
+				<th>
+				</th>
+				<td>
+				@foreach ($errorsDesc as $errorDesc)
+					<p class="error-message"> {{ $errorDesc }} </p>
+				@endforeach
+				</td>
+			</tr>
 		@endif
 		<tr> <th>　　　　説明：</th> <td><input type="text" name="desc" value="{{ old('desc') }}"></td></tr>
+
+
 		@if ($errors->has('price'))
-			<tr> <th> </th> <td>{{ $errors->first('price') }}</td></tr>
+			@php
+				$errorsPrice = $errors->get('price');
+			@endphp
+			<tr> 
+				<th>
+				</th> 
+				<td>
+				@foreach ($errorsPrice as $errorPrice)
+					<p class="error-message"> {{ $errorPrice }} </p>
+				@endforeach
+				</td>
+			</tr>
 		@endif
 		<tr> <th>　　　　価格：</th> <td><input type="number" name="price" value="{{ old('price') }}"></td></tr>
 
-		<tr> <th></th> <td><input type="submit" value="send"></td> </tr>
+		<tr> <th></th> <td><input type="submit" value="登録"></td> </tr>
 	</form>
 	</table>
 
