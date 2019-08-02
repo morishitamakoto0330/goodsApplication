624bee48227aad9fb67bbec91004052eccbeb6a7
root <root@ip-172-31-47-190.ap-northeast-1.compute.internal>
2019-08-02 18:50:33 +0000

    [update] validation check

--- app/Http/Requests/GoodRequest.php
+++ app/Http/Requests/GoodRequest.php
@@ -31,7 +31,7 @@ class GoodRequest extends FormRequest
 		    'image' => 'required|image|mimes:jpeg,png',
 		    'title' => 'required|string|strlen_max100',
 		    'desc'  => 'required|string|strlen_max500',
-		    'price' => 'required|integer',
+		    'price' => 'required|integer|min:0',
 	    ];
     }
 
@@ -43,15 +43,16 @@ class GoodRequest extends FormRequest
     public function messages()
     {
 	    return [
-		    'image.required'     => '※商品画像を選んで下さい',
-		    'image.image'        => '※画像ファイルを選んで下さい',
-		    'image.mimes'        => '※商品画像の拡張子はjpegもしくはpngにして下さい',
-		    'title.required'     => '※商品タイトルを記入してください',
+		    'image.required'      => '※商品画像を選んで下さい',
+		    'image.image'         => '※画像ファイルを選んで下さい',
+		    'image.mimes'         => '※商品画像の拡張子はjpegもしくはpngにして下さい',
+		    'title.required'      => '※商品タイトルを記入してください',
 		    'title.strlen_max100' => '※商品タイトルは100文字以内にして下さい',
-		    'desc.required'      => '※商品説明を記入して下さい',
+		    'desc.required'       => '※商品説明を記入して下さい',
 		    'desc.strlen_max500'  => '※商品説明は500文字以内にして下さい',
-		    'price.required'     => '※商品価格を設定して下さい',
-		    'price.integer'      => '※商品価格は整数で設定して下さい',
+		    'price.required'      => '※商品価格を設定して下さい',
+		    'price.integer'       => '※商品価格は整数で設定して下さい',
+		    'price.min'           => '※商品価格は0以上の正の整数で設定して下さい',
 	    ];
     }
 }
